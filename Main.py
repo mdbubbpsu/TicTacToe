cd17328e7b37a1ff7b868f58df4be1d9de23b584
mdbubbpsu <mdb5745@psu.edu>
2020-07-30 13:39:39 -0400

    updated to take user input

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ def printBoard():
     x, y = 3, 3
     board = [[0 for i in range(x)] for j in range(y)]
     count = 1
-    print("Game Board: \n")
+    print("Welcome to Tic Tac Toe! Here is your game board: \n")
     for i in range(0, len(board[0])):
         for j in range(0, len(board)):
             board[i][j] = count
@@ -10,8 +10,15 @@ def printBoard():
             count += 1
         print()
 
+
+def userInput():
+    userNum = input(" \nYou are player X please make your first move by typing in the number of the spot you wish to move into \n")
+    print(userNum)
+
 def main():
     printBoard()
+    userInput()
+
 
 
 

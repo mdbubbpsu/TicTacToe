3c9d2f16b08c66eafb6465cd6e717589c62c043c
mdbubbpsu <mdb5745@psu.edu>
2020-11-24 23:26:49 -0500

    Minimax added, very broken, needs fixed

--- Main.py
+++ Main.py
@@ -1,75 +1,197 @@
-x, y = 3, 3
-board = [[0 for i in range(x)] for j in range(y)]
-def printBoard( start):
+import copy
+
+def createBoard(rows, cols):
+    board = [[False for i in range(cols)] for j in range(rows)]
     count = 1
-    print("Welcome to Tic Tac Toe! Here is your game board: \n")
-    for i in range(0, len(board[0])):
-        for j in range(0, len(board)):
-            if start:
+    for i in range(rows):
+        for j in range(cols):
+            if count < rows * cols:
                 board[i][j] = count
-                print(board[i][j], "   ", end=" ")
                 count += 1
-            if not start:
-                print(board[i][j], "   ", end=" ")
-        print()
-    return board
-
-def placeMove(number):
-    rowNum = number //3
-    colNum = number % 3
-    if colNum == 0:
-        rowNum -= 1
-        colNum = 2
-    else:
-        colNum -= 1
-    if valid(board, rowNum,colNum):
-        board[rowNum][colNum] = 'X'
-        start = False
-        printBoard(start)
-        computerMove()
-    else:
-        print("Invalid move. Please choose again")
-        userInput()
-
-def userInput():
-    userNum = input(" \nYou are player X please make your first move by typing in the number of the spot you wish to move into \n")
-    placeMove(int(userNum))
-
-def valid(lboard, rowNum, colNum):
-    if lboard[rowNum][colNum] != 'X' and lboard[rowNum][colNum] != 'O':
-        return True
-    else:
-        return False
+            else:
+                board[i][j] = 9
+    return ticTacToe(board)
 
 
-def computerMove():
-    openSpots = 8
-    localBoard = board
+class ticTacToe(object):
 
-    if openSpots == 0:
-        #Return max points list
-        a = 2
-    else:
-        print('\n \n \n \n ')
-        for localRow in range(0,3):
-            for localCol in range(0,3):
-                if valid(localBoard, localRow,localCol):
-                    localBoard[localRow][localCol] = 'O'
-                        #CALL MINIMAX AGAIN
-                    print(localBoard[localRow][localCol], "   ", end=" ")
+    def __init__(self, board):
+        self.board = board
+        self.x = len(board)
+        self.y = len(board[0])
+
+    def getBoard(self):
+        return self.board
+
+    def solved(self):
+        db = self.board
+        one = [self.board[0][0], self.board[1][0], self.board[2][0]]
+        two = [self.board[0][1], self.board[1][1], self.board[2][1]]
+        three = [self.board[0][2], self.board[1][2], self.board[2][2]]
+
+        four = [self.board[0][0], self.board[0][1], self.board[0][2]]
+        five = [self.board[1][0], self.board[1][1], self.board[1][2]]
+        six = [self.board[2][0], self.board[2][1], self.board[2][2]]
+
+        seven = [self.board[0][0], self.board[1][1], self.board[2][2]]
+        eight = [self.board[0][2], self.board[1][1], self.board[2][0]]
+        # First column
+        if self.board[0][0] != 1 and self.board[1][0] != 4 and self.board[2][0] != 7:
+            if all(i == 'X' for i in one):
+                return True, 'X'
+            elif all(i == 'O' for i in one):
+                return True, 'O'
+            else:
+                return False, -1
+
+        # Second column
+        elif self.board[0][1] != 2 and self.board[1][1] != 5 and self.board[2][1] != 8:
+            if all(i == 'X' for i in two):
+                return True, 'X'
+            elif all(i == 'O' for i in two):
+                return True, 'O'
+            else:
+                return False, -1
+
+        # Third Column
+        elif self.board[0][2] != 3 and self.board[1][2] != 6 and self.board[2][2] != 9:
+            if all(i == 'X' for i in three):
+                return True, 'X'
+            elif all(i == 'O' for i in three):
+                return True, 'O'
+            else:
+                return False, -1
+
+        # First Row
+        elif self.board[0][0] != 1 and self.board[0][1] != 2 and self.board[0][2] != 3:
+            if all(i == 'X' for i in four):
+                return True, 'X'
+            elif all(i == 'O' for i in four):
+                return True, 'O'
+            else:
+                return False, -1
+
+        # Second Row
+        elif self.board[1][0] != 4 and self.board[1][1] != 5 and self.board[1][2] != 6:
+            if all(i == 'X' for i in five):
+                return True, 'X'
+            elif all(i == 'O' for i in five):
+                return True, 'O'
+            else:
+                return False, -1
+
+        # Third Row
+        elif self.board[2][0] != 7 and self.board[2][1] != 8 and self.board[2][2] != 9:
+            if all(i == 'X' for i in six):
+                return True, 'X'
+            elif all(i == 'O' for i in six):
+                return True, 'O'
+            else:
+                return False, -1
+
+        # First diagonal
+        elif self.board[0][0] != 1 and self.board[1][1] != 5 and self.board[2][2] != 9:
+            if all(i == 'X' for i in seven):
+                return True, 'X'
+            elif all(i == 'O' for i in seven):
+                return True, 'O'
+            else:
+                return False, -1
+
+        # Second diagonal
+        elif self.board[0][2] != 3 and self.board[1][1] != 5 and self.board[2][0] != 7:
+            if all(i == 'X' for i in eight):
+                return True, 'X'
+            elif all(i == 'O' for i in eight):
+                return True, 'O'
+            else:
+                return False, -1
+        else:
+            return False, -1
+
+    def displayBoard(self, board):
+        for i in range(self.x):
+            for j in range(self.y):
+                if j == 0:
+                    print("|", board[i][j], " | ", end="")
                 else:
-                    print(localBoard[localRow][localCol], "   ", end=" ")
-
+                    print(board[i][j], " | ", end="")
             print()
 
+    def valid(self, row, col):
+        if self.board[row][col] != 'X' and self.board[row][col] != 'O':
+            return True
+        else:
+            return False
+
+    def performMove(self, row, col, turn):
+        if self.valid(row, col):
+            self.board[row][col] = turn
+            self.displayBoard(self.board)
+            return True
+        else:
+            print("INVALID MOVE, PLEASE TRY AGAIN")
+            self.displayBoard(self.board)
+            return False
+
+    def copy(self):
+        return ticTacToe(copy.deepcopy(self.board))
+
+    def successors(self, turn):
+        for i in range(self.x):
+            for j in range(self.y):
+                copyBoard = self.copy()
+                if copyBoard.performMove(i, j, turn):
+                    yield (i, j), copyBoard
+
+    def get_best_move(self, board):
+        startBoard = self
+        value, move = self.maxValue(startBoard)
+        return value, move
+
+    def maxValue(self, board):
+        boo, turn = board.solved()
+        util = 0
+        if boo:
+            if turn == 'X':
+                util = -1
+            elif turn == 'O':
+                util = 1
+            return util, None
+        v = -1000
+        returnIndex = (0, 0)
+        for ind, newBoard in board.successors('O'):
+            v2, a2 = self.minValue(newBoard)
+            if v2 > v:
+                v, returnIndex = v2, ind
+        return v, returnIndex
+
+
+    def minValue(self, board):
+        boo, turn = board.solved()
+        util = 0
+        if boo:
+            if turn == 'X':
+                util = 1
+            elif turn == 'O':
+                util = -1
+            return util, None
+        v = 1000
+        returnIndex = (0, 0)
+        for ind, newBoard in board.successors('X'):
+            v2, a2 = self.maxValue(newBoard)
+            if v2 < v:
+                v, returnIndex = v2, ind
+        return v, returnIndex
+
+
+b = createBoard(3,3)
+#b.displayBoard(b.getBoard())
+print(b.get_best_move(b))
 
 
 
 
-def main():
-    start = True
-    board = printBoard(start)
-    userInput()
 
 
 
@@ -78,5 +200,18 @@ def main():
 
 
 
-if __name__ == '__main__':
-    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
